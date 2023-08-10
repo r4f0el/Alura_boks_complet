baf49b0d7f20c09d929d33fafd0045254389109f
r4f0el <107858193+r4f0el@users.noreply.github.com>
2023-08-10 11:28:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+
+    try {
+        var consultaCep = await fetch (`https://viacep.com.br/ws/${cep}/json/`)
+        var consultaCepConvertida = await consultaCep.json()
+        if (consultaCepConvertida.erro) {
+            throw Error('O CEP informado não existe!');
+        }
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+        var bairro = document.getElementById('bairro');
+
+        cidade.value = consultaCepConvertida.localidade;
+        logradouro.value = consultaCepConvertida.logradouro;
+        estado.value = consultaCepConvertida.uf;
+        bairro.value = consultaCepConvertida.bairro;
+
+
+        console.log(consultaCepConvertida);
+        return consultaCepConvertida;
+    } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP inválido, tente novamente!</p>`
+        console.log(erro)
+    }
+    
+}
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
